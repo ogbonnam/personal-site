f5eb3cc7ec819e49175632fc48b577eaa3a35269
Ecommerce <ecommerce@BMGWA-LAPTOP.Nigerinsurance.com>
2020-05-14 13:45:20 +0100

    Commit Code for my Personal Site and my RevealOnScroll

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,10 @@
+import '../styles/style.css'
+import MobileMenu from './modules/MobileMenu'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+let mobileMenu = new MobileMenu();
+let revealOnScroll = new RevealOnScroll();
+
+if(module.hot){
+    module.hot.accept()
+}
---
+++ app/assets/scripts/modules/MobileMenu.js
@@ -0,0 +1,20 @@
+class MobileMenu{
+   constructor(){
+       this.menuIcon = document.querySelector('.header__menu-icon')
+       this.menuContent = document.querySelector('.header__menu-content')
+       this.header = document.querySelector('.header')
+       this.events()
+   }
+
+   events(){
+       this.menuIcon.addEventListener('click', () => this.toggleTheMenu())
+   }
+
+   toggleTheMenu(){
+       this.menuContent.classList.toggle('header__menu-content--is-visible')
+       this.header.classList.toggle('header--is-expanded')
+       this.menuIcon.classList.toggle("header__menu-icon__close-x")
+   }
+}
+
+export default MobileMenu;
